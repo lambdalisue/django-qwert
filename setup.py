5d51391ae6c6c6bfdb7b1567227d307640181032
Λlisue <lambdalisue@hashnote.net>
2011-12-15 02:26:29 +0900

    Add required

--- setup.py
+++ setup.py
@@ -33,5 +33,11 @@ setup(
         packages = find_packages(),
         include_package_data = True,
         zip_safe = True,
-        install_requires=['setuptools', 'markdown', 'BeautifulSoup', 'Akismet'],
+        install_requires=[
+            'setuptools', 
+            'setuptools-git',
+            'markdown', 
+            'BeautifulSoup', 
+            'Akismet'
+        ],
 )
